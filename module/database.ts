8beaf0592c40e8bf9cce237325b2798b42ac648c
Michael Polesensky <adrianpolesensky@gmail.com>
2022-01-25 13:53:00 +0100

    Big additions to the project.

---
+++ module/database.ts
@@ -0,0 +1,35 @@
+import {Database} from "sqlite3";
+import {Response} from "express";
+
+export function queryProducts(type: string, query: string, db: Database, response: Response): void {
+    if (type === "all") {
+        db.all(query, [], (err, rows) => {
+            if (err) {
+                console.error("Fetch failed: " + err)
+                console.error("GET 400: BAD REQUEST")
+                return
+            }
+
+            console.log("Query OK")
+            rows.forEach(row => {response.send(row)})
+        })
+    } else if (type === "single") {
+        db.get(query, [], (err, row) => {
+            if (err) {
+                console.error("Fetch failed: " + err)
+                return
+            }
+
+            console.log("Query OK")
+            response.send(row)
+        })
+    }
+}
+
+export function prepare(db: Database): void {
+    db.serialize(function () {
+        db.run(`CREATE TABLE products (id INTEGER PRIMARY KEY AUTOINCREMENT, brand TEXT NOT NULL, model TEXT NOT NULL,
+        os TEXT NOT NULL, screen_size INTEGER DEFAULT 10, image TEXT NOT NULL)`).run(`INSERT INTO products (id, brand, model, os, screen_size, image)
+        VALUES (0, 'Apple', 'iPhone 13', 'iOS', 17, 'https://...')`)
+    })
+}
---
+++ server/server.js
@@ -0,0 +1,65 @@
+// entry point for server
+const sqlite3 = require("sqlite3").verbose()
+const database = require("../module/database")
+const express = require("express")
+const path = require("path")
+const app = express()
+const PORT = 8080
+
+const HTTPRequestCodes = {
+    success: 200,
+    err_not_found: 404,
+    redirect: 302
+}
+
+let db = new sqlite3.Database("./private/api.db", err => {
+    if (err) {
+        console.error("Error has occurred!")
+        console.log(err)
+        return
+    }
+
+    console.log("SQLite Database successfully created/connected!")
+})
+
+// get method used to initialize the database upon creation, not used in production.
+app.get("/apiv1/init", (req, res) => {
+    database.prepare(db)
+    res.send("Database prepared")
+    console.log(`GET ${HTTPRequestCodes.success}: OK`)
+})
+
+// router
+app.get("/", (req, res) => {
+    res.redirect("/apiv1")
+    console.log(`GET ${HTTPRequestCodes.redirect}: REDIRECT`)
+})
+
+app.get("/apiv1", (req, res) => {
+    res.set({"content-type": "text/html"})
+    res.sendFile(path.join(__dirname + "/public/homePage.html"), error => {
+        if (error) {
+            console.error(`GET ${HTTPRequestCodes.err_not_found}: NOT FOUND`)
+            res.send("404: NOT FOUND")
+            return
+        }
+
+        console.log(`GET ${HTTPRequestCodes.success}: OK`)
+    })
+})
+
+app.get("/apiv1/products", (req, res) => {
+    res.set({"content-type": "application/json"})
+    database.queryProducts("all", "SELECT ID id, Brand brand, Model model, OS os, Screen_Size screen_size, Image image FROM products ORDER BY id", db, res)
+    console.log(`Get ${HTTPRequestCodes.success}: OK`)
+})
+
+app.get("/apiv1/products/:id", (req, res) => {
+    res.set({"content-type": "application/json"})
+    database.queryProducts("single", `SELECT ID id, Brand brand, Model model, OS os, Screen_Size screen_size, Image image FROM products WHERE id = ${req.params.id}`, db, res)
+    console.log(`GET ${HTTPRequestCodes.success}: OK`)
+})
+
+app.listen(PORT, function () {
+    console.log(`Express app listening on ${PORT}`)
+})
