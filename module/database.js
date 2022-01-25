6e88c6618f41847c33860d4b791d14538f4da136
Michael Polesensky <adrianpolesensky@gmail.com>
2022-01-25 13:54:57 +0100

    Big additions to the project.

---
+++ module/database.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.prepare = exports.queryProducts = void 0;
+function queryProducts(type, query, db, response) {
+    if (type === "all") {
+        db.all(query, [], (err, rows) => {
+            if (err) {
+                console.error("Fetch failed: " + err);
+                return;
+            }
+            console.log("Query OK", rows);
+            response.send(rows);
+        });
+    }
+    else if (type === "single") {
+        db.get(query, [], (err, row) => {
+            if (err) {
+                console.error("Fetch failed: " + err);
+                return;
+            }
+            console.log("Query OK");
+            response.send(row);
+        });
+    }
+}
+exports.queryProducts = queryProducts;
+function prepare(db) {
+    db.serialize(function () {
+        db.run(`CREATE TABLE products (id INTEGER PRIMARY KEY AUTOINCREMENT, brand TEXT NOT NULL, model TEXT NOT NULL,
+        os TEXT NOT NULL, screen_size INTEGER DEFAULT 10, image TEXT NOT NULL)`).run(`INSERT INTO products (id, brand, model, os, screen_size, image)
+        VALUES (0, 'Apple', 'iPhone 13', 'iOS', 17, 'https://...')`);
+    });
+}
+exports.prepare = prepare;
